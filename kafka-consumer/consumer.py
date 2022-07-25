a6f6ef7d1a8a5fc51447644e2fd20099c7414ae1
Lee Hyungi <hyungi.lee.622@gmail.com>
2022-07-26 02:36:55 +0900

    feat: Kafka consumer Python script 정리

--- kafka-consumer/consumer.py
+++ kafka-consumer/consumer.py
@@ -1,21 +1,24 @@
-from json import loads
+
+from dotenv import load_dotenv
+import os
 from kafka import KafkaConsumer
-from time import sleep
+
+# load .env
+load_dotenv()
 
 #Topic
 topic_name = "twitter"
 
 consumer = KafkaConsumer(
     topic_name,
-    bootstrap_servers = ['192.168.200.169:9091'],
+    bootstrap_servers = ["{}:9091".format(os.environ.get("bootstrap_servers_ip"))],
     auto_offset_reset='earliest',
     group_id = 'group1',
     enable_auto_commit = True,
-    consumer_timeout_ms = 50000
+    consumer_timeout_ms = 10000
 )
 
 for message in consumer:
-    print(message)
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+    print ("[consumer] %s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value.decode('utf-8')))
