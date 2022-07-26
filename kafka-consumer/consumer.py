adeaf6765440d58b72d25738af62683316c66e54
Lee Hyungi <hyungi.lee.622@gmail.com>
2022-07-26 16:41:36 +0900

    feat: kafka-consumer/consumer.py Python script refactoring

--- kafka-consumer/consumer.py
+++ kafka-consumer/consumer.py
@@ -1,24 +1,32 @@
-
 from dotenv import load_dotenv
 import os
 from kafka import KafkaConsumer
+# consumer -> ELK docker container로 연결
+import logging
+from logging.config import fileConfig
 
 # load .env
 load_dotenv()
 
-#Topic
-topic_name = "twitter"
-
-consumer = KafkaConsumer(
+def getConsumerObj():
+    consumerObj = KafkaConsumer(
     topic_name,
     bootstrap_servers = ["{}:9091".format(os.environ.get("bootstrap_servers_ip"))],
     auto_offset_reset='earliest',
     group_id = 'group1',
     enable_auto_commit = True,
     consumer_timeout_ms = 10000
-)
+    )
 
-for message in consumer:
-    print ("[consumer] %s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value.decode('utf-8')))
+    return consumerObj
+
+if __name__ == '__main__':
+    #Topic
+    topic_name = "twitter"
+    # Get consumer object from getConsumerObj method
+    consumer = getConsumerObj()
+
+    for message in consumer:
+        print ("[consumer] %s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                            message.offset, message.key,
+                                            message.value.decode('utf-8')))
